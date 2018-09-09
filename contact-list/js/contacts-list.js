207ee87f3cf4b535fb683125a367813f0cfe29d5
Хачатур <>
2018-09-09 21:57:58 +0300

    HW dom

---
+++ contact-list/js/contacts-list.js
@@ -0,0 +1,9 @@
+
+const contactsList = document.querySelector('.contacts-list');
+const dataList = JSON.parse(loadContacts());
+
+for (let item of dataList) {
+  contactsList.innerHTML += `<li data-email="${item.email}" data-phone="${item.phone}">
+                              <strong>${item.name}</strong>
+                            </li>`;
+}
